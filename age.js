90eed3c225d0fb2db6395ab897b14b19bd68d651
mnangwe <pupuma.azabenathi@gmail.com>
2022-01-03 07:39:33 +0200

    style update

--- age.js
+++ age.js
@@ -1,16 +1,17 @@
 function ageCalc() {
-    var currentYear = document.getElementById('currentYear').value;
-    var bornYear = document.getElementById('bornYear').value;
-    var age = currentYear - bornYear
+    let currentYear = document.getElementById('currentYear').value;
+    let bornYear = document.getElementById('bornYear').value;
+    let age = currentYear - bornYear
+    let date = new Date()
 
-    if (currentYear < bornYear || currentYear != 2021) {
-        alert('You entered a wrong year')
+    if (currentYear == '' || bornYear == '') {
+        alert('Please ensure that you filled up the spaces')
     }
-    else if (isNaN(currentYear)) {
+    else if (isNaN(currentYear) || isNaN(bornYear)) {
         alert('Please write numbers only')
     }
-    else if (isNaN(bornYear)) {
-        alert('Please write numbers only')
+    else if (currentYear < bornYear || currentYear != date.getFullYear()) {
+        alert('Please enter correct details')
     }
     else {
         alert('Your age is: ' + age)
@@ -21,11 +22,26 @@ function ageCalc() {
 
 
 function tempCalc(){
-    var temperature = document.getElementById('Temperature').value
-    var celsius = (temperature-32)*9/5
-    alert(`The temperature is ${celsius} degrees celsius`)
-    if(celsius > 23){ alert("It's warm today wear shorts and T-shrits")}
-    else if(celsius < 22 && celsius > 16){alert("Wear jeans and long sleeves")}
-    else if(celsius < 16 && celsius > 12){alert("It's going to be cold today")}
-    else{alert("Becareful of the possible rain today")}
+    let temperature = document.getElementById('Temperature').value
+    let celsius = (temperature-32)*9/5
+
+
+    if (temperature == '') {
+        alert('Please enter temperature')
+    }
+    else if (isNaN(temperature)) {
+        alert('Please write numbers only')
+    }
+    else if(celsius > 23){
+        alert("The temperature is ${celsius} degrees. It's warm today wear shorts and T-shrits")
+    }
+    else if(celsius < 22 && celsius > 16){
+        alert("The temperature is ${celsius} degrees. Wear jeans and long sleeves")
+    }
+    else if(celsius < 16 && celsius > 12){
+        alert("The temperature is ${celsius} degrees. It's going to be cold today")
+    }
+    else{
+        alert("The temperature is ${celsius} degrees. Becareful of the possible rain today")
+    }
 }
